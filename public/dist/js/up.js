f5ba2fa05ae2799729e83bad86b2c0530cbb56de
kaira <husaynmozart@gmail.com>
2019-06-17 10:30:24 +0300

    ajax notification

--- public/dist/js/up.js
+++ public/dist/js/up.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
-    /*function notification(){
+    function notification(){
         // mise en place d'ajax pour récupérer le nombre de message recue
-        let routage = "{{ path('notif_non')|escape('js') }}"
+        let routage = $('#notif_non').val()
+        //let routage = 'http://127.0.0.1:8000/notif_non';
         $.get(routage, function(data){
             // Une ou plusieurs instructions
-            $('#non').innerText(data['dataResponse']);
+            $('#non').text(data['dataResponse']);
+            //alert(data['dataResponse']);
         });
     
         // mise à jout affichage
         
     }
-	setInterval(notification, 10000);*/
+	setInterval(notification, 10000);
     //var routing = "{{ path('upload_file') }}";
     //var routing = "127.0.0.1:8000/upload_file";
     var routing = $('#upload_file').val();
